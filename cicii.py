08f784c38b7095f4d05effb6e59622b796921100
Nilam-here <amanda.desi.2203226@students.um.ac.id>
2024-09-09 10:48:08 +0700

    Update cicii.py

--- cicii.py
+++ cicii.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
  
 # Header
-st.header('Amanda Desi :sparkles:')
+st.header('Amanda :sparkles:')
 st.subheader('Plot')
  
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
@@ -57,4 +57,3 @@ with col2:
     st.pyplot(fig)  
  
 st.caption('Copyright © Nugroho Adi Pramono 2023')
-
