08daa1e9441a7f0cf69793d0c05b2e479abf3bd8
Nilam-here <amanda.desi.2203226@students.um.ac.id>
2024-09-23 10:08:13 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -3,8 +3,13 @@ import streamlit as st
 x =  st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write ("Anda masukkan", x,' ',sx)
-sy = st.text_input("Dikonversi ke", "F")
-Fahrenheit = (9/5) * x + 32
+sy = st.text_input("Dikonversi ke", "C")
+y = 0
+if (sx == 'C'):
+  if (sy == 'C'):
+    y=x
+  elif(sy == 'F'):
+    Fahrenheit = (9/5) * x + 32
 st.write (x,'',sx,'=...', sy)
 
   
