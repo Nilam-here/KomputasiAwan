95b47196e1fb0685abd46200b14c52e68d92e64d
Nilam-here <amanda.desi.2203226@students.um.ac.id>
2024-09-23 10:30:20 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -3,13 +3,17 @@ import streamlit as st
 x =  st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write ("Anda masukkan", x,' ',sx)
-sy = st.text_input("Dikonversi ke", "F")
+sy = st.number_input("Dikonversi ke", "Masukkan satuan")
 y = 0
 if (sx == 'C'):
   if (sy == 'C'):
     y=x
   elif(sy == 'F'):
     (9/5) * x + 32
+  elif(sy == 'K'):
+    x+273.15
+  elif(sy == 'R'):
+    x*0.8
 st.write (x,'',sx,'=...', sy)
 
   
