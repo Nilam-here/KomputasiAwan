e1ced36fcc7ea73317adeae7a18b59bc170ca2a3
Nilam-here <amanda.desi.2203226@students.um.ac.id>
2024-09-23 09:47:08 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,11 +1,11 @@
 import steamlit as st
 
 x =  st.number_input(
-    "Insert a namber", value=None, placeholder="Type a number...")
+    "14", value=None, placeholder="Type a number...")
 )
-st.write("The current number is ", x)
+st.write("14", x)
 st.latex(r'''
     x^2 = 
     ...)
-st.write(x=x)
+st.write(x*x)
   
